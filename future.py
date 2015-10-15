55a27a4a21f37ed911592e35ed1fe2c2f1265552
liuge1994 <610562301@qq.com>
2015-10-15 22:12:24 +0800

    future Python3x

---
+++ future.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#still running on Python2.7
+
+from __future__ import unicode_literals
+
+from __future__ import division
+
+print '\'xxx\' is unicode?', isinstance('xxx', unicode)
+
+print 'u\'xxx\' is unicode?', isinstance(u'xxx', unicode)
+
+print '\'xxx\' is str?', isinstance('xxx', str)
+
+print 'b\'xxx\' is str?', isinstance(b'xxx', str)
+
+print '10 / 3 = ', 10 / 3
+print '10.0 / 3 = ', 10.0 / 3
+print '10 // 3 = ', 10 // 3   #on Python2.7 // = /
+
