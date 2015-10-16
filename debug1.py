6911ff20a9984b2e120b6466147b6cd4309c74b9
liuge1994 <610562301@qq.com>
2015-10-16 21:40:07 +0800

    second use try

--- debug1.py
+++ debug1.py
@@ -24,4 +24,16 @@ finally:
     print 'finally...'
 print 'END'
 
+try:
+    print 'try...'
+    r = 10 / int('a')
+    print 'result: ', r
+except ValueError, e:
+    print 'ValueError: ', e
+except ZeroDivisionError, e:
+    print 'ZeroDivisionError: ', e
+finally:
+    print 'finally...'
+print 'END'
+
 
