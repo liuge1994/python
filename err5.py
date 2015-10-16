3dceec91f27e3930be642afeae6fb77d272f27a5
liuge1994 <610562301@qq.com>
2015-10-16 22:59:46 +0800

    the use of pdb

---
+++ err5.py
@@ -0,0 +1,14 @@
+#the use of pdb
+
+#python -m pdb err5.py
+
+#input 'l' view the code
+#input 'n' step bu step excute the code
+#input 'p para' to view the parameter
+#input 'q' quit
+ 
+
+s = '0'
+n = int(s)
+print 10 / n
+
