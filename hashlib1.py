d8d5465969f4de90a4a112674474284326c3f0db
liuge1994 <610562301@qq.com>
2015-10-18 14:39:10 +0800

    hashlib

---
+++ hashlib1.py
@@ -0,0 +1,18 @@
+import hashlib
+
+md5 = hashlib.md5()
+
+md5.update('how to use md5 in python hashlib?')
+
+print md5.hexdigest()
+
+md5 = hashlib.md5()
+md5.update('how to use md5 in ')
+md5.update('python hashlib?')
+print md5.hexdigest()
+
+sha1 = hashlib.sha1()
+sha1.update('how to use sha1 in python hashlib?')
+
+print sha1.hexdigest()
+
