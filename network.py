ac47b58f67db07c134c464cf4cd917e053209fd8
liuge1994 <610562301@qq.com>
2015-10-19 20:48:12 +0800

    network

--- network.py
+++ network.py
@@ -4,7 +4,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect(('www.sina.com.cn',80))
 
-s.send(b'GET/HTTP/1.1\r\nHost: www.sina.com.cn\r\nConnection:close\r\n\r\n')
+s.send('GET/HTTP/1.1\r\nHost: www.sina.com.cn\r\nConnection:close\r\n\r\n')
 
 buffer = []
 
@@ -18,3 +18,8 @@ data = b''.join(buffer)
 
 s.close()
 
+header, html = data.split(b'\r\n\r\n', 1)
+print(header.decode('utf-8'))
+
+with open('sina.html', 'Wb') as f:
+    f.write(html)
